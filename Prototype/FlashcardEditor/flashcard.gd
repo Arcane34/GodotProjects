4132d4bacc55bce2bb71e12a230d1f6b22a5b34d
Aarogya Yadav <aky104@student.bham.ac.uk>
2023-09-09 21:11:48 +0100

    Created an editor scene to add and load flashcards

---
+++ Prototype/FlashcardEditor/flashcard.gd
@@ -0,0 +1,6 @@
+extends Control
+
+
+
+func _on_delete_pressed():
+	queue_free()
--- Prototype/FlashcardEditor/save_card.gd
+++ Prototype/FlashcardEditor/save_card.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
 const SAVE_FILE = "user://save_file.dat"
-var g_data = {}
-@onready var Cfront = $CardFront
-@onready var Cback = $CardBack
+var g_data = []
+@onready var cards = $ScrollContainer/CardContainer
+
+
 
 func _ready():
 	load_data()
@@ -15,15 +16,19 @@ func save_data():
 	
 func load_data():
 	if not FileAccess.file_exists(SAVE_FILE):
-		g_data = {
-			"front": "",
-			"back":""
-		}
+		g_data = [[],[]]
 		save_data()
 	var file = FileAccess.open(SAVE_FILE, FileAccess.READ)
-	var g_data = file.get_var()
-	Cfront.text = g_data.front
-	Cback.text = g_data.back
+	g_data = file.get_var()
+	if not g_data.is_empty():
+		var scene = load("res://FlashcardEditor/flashcard.tscn")
+	
+		for i in len(g_data[0]):
+			var instance = scene.instantiate()
+			instance.get_node("HBoxContainer/Front").text = g_data[0][i]
+			instance.get_node("HBoxContainer/Back").text = g_data[1][i]
+			cards.add_child(instance)
+	
 	file.close()
 	print(g_data)
 	print("lol")
@@ -31,6 +36,17 @@ func load_data():
 
 
 func _on_button_pressed():
-	g_data.front = Cfront.text
-	g_data.back = Cback.text
+	g_data = [[],[]]
+	var cardObjects = cards.get_children()
+	for i in len(cardObjects):
+		g_data[0].append(cardObjects[i].get_node("HBoxContainer/Front").text)
+		g_data[1].append(cardObjects[i].get_node("HBoxContainer/Back").text)
+	
 	save_data()
+
+
+func _on_add_pressed():
+	var scene = load("res://FlashcardEditor/flashcard.tscn")
+	var instance = scene.instantiate()
+	cards.add_child(instance)
+	
